e26da9c7dfe3df7f90d7dc33e60b7948ddc97c0d
Nathan Lewis <n.lewis.perc@gmail.com>
2020-11-26 20:51:43 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -2,12 +2,15 @@ class Dog
   def name=(dogs_name)
     @this_dogs_name = dogs_name
   end
+  
   def name
     @this_dogs_name
   end
+  
   def breed=(dogs_breed)
     @this_dogs_breed = dogs_breed
   end
+  
   def breed
     @this_dogs_breed
   end
